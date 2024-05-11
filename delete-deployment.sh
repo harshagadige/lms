cb2afca2a57555a88bfeabde4c1c9f3e623bc47e
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-05-12 00:03:57 +0530

    Create delete-deployment.sh

---
+++ delete-deployment.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# remove previous script ( lms-deploy.sh) output
+sudo rm -rf lms
+
+# Stop Nginx service
+sudo systemctl stop nginx
+
+# Remove Nginx
+sudo apt-get purge nginx nginx-common nginx-full nginx-core -y
+sudo apt-get autoremove --purge -y
+sudo apt-get autoclean
+sudo apt-get remove --purge -y
+
+# Remove Node.js
+sudo apt-get remove nodejs npm -y
+
+# Remove PostgreSQL without prompts and without purging
+sudo DEBIAN_FRONTEND=noninteractive apt-get remove postgresql* --no-purge -y
+sudo apt-get autoremove --purge -y
+sudo apt-get autoclean
+
+echo "lms, Nginx, Node.js, and PostgreSQL removed successfully!"
